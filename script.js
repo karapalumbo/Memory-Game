4c012b00be076bb64b936586cbc391096a26c614
Kara <palumbokara@yahoo.com>
2020-09-14 12:02:21 -0600

    First Commit of Memory Game

---
+++ script.js
@@ -0,0 +1,105 @@
+const gameContainer = document.getElementById("game");
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+];
+
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
+
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
+    newDiv.classList.add("hide");
+    newDiv.setAttribute("data-color", color);
+
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+let openCards = [];
+let counter = 0;
+
+// TODO: Implement this function!
+function handleCardClick(event) {
+  event.target.classList.toggle("hide"); //white
+  event.target.classList.add("clicked");
+  openCards.push(event.target); // push cards to array
+  if (openCards.length === 2) {
+    // if array = 2 cards
+    if (
+      // if array cards equal each other
+      openCards[0].getAttribute("data-color") ===
+      openCards[1].getAttribute("data-color")
+    ) {
+      counter++;
+      setTimeout(function () {
+        if (counter === 5) {
+          alert("You won the game!");
+        }
+      }, 500);
+      //disable click on each card
+      openCards[0].style.pointerEvents = "none";
+      openCards[1].style.pointerEvents = "none";
+    } else {
+      //disable click
+      let firstUnmatched = openCards[0];
+      let secondUnmatched = openCards[1];
+      firstUnmatched.classList.remove("clicked");
+      secondUnmatched.classList.remove("clicked");
+      gameContainer.style.pointerEvents = "none";
+      setTimeout(function () {
+        // unmatched = white
+        firstUnmatched.classList.toggle("hide");
+        secondUnmatched.classList.toggle("hide");
+        gameContainer.style.pointerEvents = "auto"; //enable click
+      }, 1000);
+    }
+    openCards = [];
+  }
+}
+
+// when the DOM loads
+createDivsForColors(shuffledColors);
